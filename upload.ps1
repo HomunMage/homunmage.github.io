f805633af623b4cd5ce094ae0606973950a3596a
AIMageGuild <AIMageGuild@users.noreply.github.com>
2024-02-02 12:19:11 +0800

    upload

--- upload.ps1
+++ upload.ps1
@@ -1,6 +1,12 @@
+cd _shared
+./upload.bat
+cd ../
+
 git config --local user.name "AIMageGuild"
 git config --local user.email "AIMageGuild@users.noreply.github.com"
 git remote set-url origin git@AI:aimageguild/aimageguild.github.io.git
+
+git submodule update --recursive --remote
 
 git pull
 git add .
