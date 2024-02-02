5e8c9123577d223b70bde0d156a6d99b232973cc
AIMageGuild <AIMageGuild@users.noreply.github.com>
2024-02-02 12:10:09 +0800

    upload

--- upload.bat
+++ upload.bat
@@ -1,6 +1,6 @@
 git config --local user.name "AIMageGuild"
 git config --local user.email "AIMageGuild@users.noreply.github.com"
-git remote set-url origin git@AI:aimageguild/Practicing.git
+git remote set-url origin git@AI:aimageguild/aimageguild.github.io.git
 
 git pull
 git add .
