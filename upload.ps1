d08422ba6612e61568d5cfe059e51fe7a8a87aeb
HomunMage <homun@posetmage.com>
2024-04-25 12:19:59 +0800

    upload

--- upload.ps1
+++ upload.ps1
@@ -12,7 +12,7 @@ cd ../
 
 git config --local user.name "HomunMage"
 git config --local user.email "homun@posetmage.com"
-git remote set-url origin git@AI:homunmage/homunmage.github.io.git
+git remote set-url origin git@HO:homunmage/homunmage.github.io.git
 
 git submodule update --recursive --remote
 
